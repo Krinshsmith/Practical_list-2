33749e71f44379885825c1a69c15336374a6906a
Krinshsmith <141899936+Krinshsmith@users.noreply.github.com>
2023-09-29 21:33:19 +0530

    Add files via upload

---
+++ p7.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int si,p,r,t;
+	
+	cout<<"My Name Is Krinshsmith Kava"<<endl;
+	cout<<"En_no    220130318102"<<endl;
+	
+	cout<<"Enter the principle amount  : ";
+	cin>>p;
+	cout<<"Enter the Rate           : ";
+	cin>>r;
+	cout<<"Enter the time           : ";
+	cin>>t;
+	
+	cout<<"This is simple interset  : "<<p*r*t/100;
+	
+}
